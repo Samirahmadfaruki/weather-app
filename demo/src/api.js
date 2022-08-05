336b613ddc7196bb0bba69c990df293122099b05
pankajyadav_pro <pankajyadav8385@gmail.com>
2022-08-05 18:08:13 +0530

    search for city

---
+++ demo/src/api.js
@@ -0,0 +1,10 @@
+export const geoApioptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '51ca884de1msha96d24f0f5308acp10624djsnc7efee8535d9',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo"
+  
+  
